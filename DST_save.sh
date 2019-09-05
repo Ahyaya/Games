65e1117545f63d7800b6a7e91e5534b10990da40
Chan <40376613+Ahyaya@users.noreply.github.com>
2019-09-05 22:17:33 +0800

    DST auto backup

--- DST_save.sh
+++ DST_save.sh
@@ -2,4 +2,5 @@
 current_Tick=$(date +%Y%m%d%H%M)
 cd ~/.klei/DoNotStarveTogether
 zip -r "DST_Cluster_${current_Tick}.zip" Cluster_1
-echo -e "\n\n\nDST shell: new zip has been saved, tick = ${current_Tick}\n\n\n"
+mv "DST_Cluster_${current_Tick}.zip" ~/backup/
+echo -e "\n\n\nDST shell: new zip has been saved in ~/backup, tick = ${current_Tick}\n\n\n"
