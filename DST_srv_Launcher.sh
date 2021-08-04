6da7e90a6ee33ee2b75f7c5e8923acc1afe31006
Chan <40376613+Ahyaya@users.noreply.github.com>
2021-08-04 11:36:17 +0800

    minor bug fixed

--- DST_srv_Launcher.sh
+++ DST_srv_Launcher.sh
@@ -8,7 +8,7 @@ screen -dmS 'DST_srv_Caves' bash -c './dontstarve_dedicated_server_nullrenderer 
 
 echo "Don't Starve Together's world initiating"
 echo "=============================================================="
-echo -e "Two screen sessions are activated, use command:\n\t'screen -r -S DST_srv_Master'\nand\n\t'screen -r -S DST_srv_Caves'\nto rettach the sessions"
+echo -e "Two screen sessions are activated, use command:\n\t'screen -r DST_srv_Master'\nand\n\t'screen -r DST_srv_Caves'\nto rettach the sessions"
 echo "=============================================================="
 
 cd ~
